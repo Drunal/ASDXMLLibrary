701c2e50d7f625dd6904618169dfb14faed6e0cd
Drunal <Drunal@users.noreply.github.com>
2016-05-21 14:51:56 +0200

    added namespace to serialization

--- ASDXMLLibrary/ContentManager.cs
+++ ASDXMLLibrary/ContentManager.cs
@@ -8,10 +8,24 @@ namespace AsdXMLLibrary
 {
     public static class ContentManager
     {
+        private static XmlSerializerNamespaces AsdNamespaces = new XmlSerializerNamespaces();
+
+        static ContentManager()
+        {
+            AsdNamespaces.Add(string.Empty, "http://www.asd-europe.org/s-series/s3000l");
+        }
+
         public static void SerializeToFile<T>(T serializableObject, string filePath)
         {
             using (var writer = new FileStream(filePath, FileMode.Create))
                 SerializeToStream<T>(serializableObject, writer);
+        }
+
+        public static void SerializeToStream<T>(T serializableObject, Stream stream)
+        {
+            // TODO: move default namespace to parameter to handle s3000l/s2000m files
+            var serializer = new XmlSerializer(typeof(T), "http://www.asd-europe.org/s-series/s3000l");
+            serializer.Serialize(stream, serializableObject, AsdNamespaces);
         }
 
         public static T DeserializeFromFile<T>(string filePath)
@@ -23,15 +37,10 @@ namespace AsdXMLLibrary
             return result;
         }
 
-        public static void SerializeToStream<T>(T serializableObject, Stream stream)
-        {
-            var serializer = new XmlSerializer(typeof(T));
-            serializer.Serialize(stream, serializableObject);
-        }
-
         public static T DeserializeFromStream<T>(Stream stream)
         {
-            var serializer = new XmlSerializer(typeof(T));
+            // TODO: move default namespace to parameter to handle s3000l/s2000m files
+            var serializer = new XmlSerializer(typeof(T), "http://www.asd-europe.org/s-series/s3000l");
             return (T)serializer.Deserialize(stream);
         }
     }
